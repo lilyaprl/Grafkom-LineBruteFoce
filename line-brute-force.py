17c239993e11672a04913ecfc92ac99b869ffd7d
Jakamesai <77713676+Jakamesai@users.noreply.github.com>
2021-12-08 20:12:22 +0800

    Update line-brute-force.py

--- line-brute-force.py
+++ line-brute-force.py
@@ -7,35 +7,39 @@ plt.subplots_adjust(bottom=0.2)
 x = np.array([3,9])
 y = np.array([1,5])
 
-m=0
-x = np.array([3,9])
-y = np.array([1,5])
-xline,yline = [],[]
-if x[0]==x[1]:
+def connect():
+    ax.cla()
+    ax.grid()
+    
+    global x,y,l0,l1,m,xline,yline
+
+    xline,yline = [],[]
+    m = 0
+    if x[0]==x[1]:
         p = y[0]
         if y[0]>y[1]:
             p = y[1]
         xline = np.append(x,x[0])
         yline = np.append(y,p+1)
-elif y[0]==y[1]:
-    m = np.Infinity
-    p = x[0]
-    if x[0]>x[1]:
-        p = y[1]
-    xline = np.append(x,(x[0])+1)
-    yline = np.append(y,y[1])
-else:
-    if m<1:
-        m = (y[1]-y[0])/(x[1]-x[0])
-        n = abs(x[1]-x[0])
-        x2 = x[0]
-        i = 1
-        while i <= n+1:
-            y2 = y[0]+m*(x2-x[0])
-            xline.append(x2)
-            yline.append(round(y2))
-            x2+=1
-            i+=1
+    elif y[0]==y[1]:
+        m = np.Infinity
+        p = x[0]
+        if x[0]>x[1]:
+            p = y[1]
+        xline = np.append(x,(x[0])+1)
+        yline = np.append(y,y[1])
+    else:
+        if m<1:
+            m = (y[1]-y[0])/(x[1]-x[0])
+            n = abs(x[1]-x[0])
+            x2 = x[0]
+            i = 1
+            while i <= n+1:
+                y2 = y[0]+m*(x2-x[0])
+                xline.append(x2)
+                yline.append(round(y2))
+                x2+=1
+                i+=1
         else:
             m = (x[1]-x[0])/(y[1]-y[0])
             n = abs(y[1]-y[0])
@@ -47,6 +51,7 @@ else:
                 xline.append(round(x2))
                 y2+=1
                 i+=1
+connect()
 np.array(xline)
 np.array(yline)
 print('Titik Koordinat: (', x[0], ',',y[0],') (',x[1],',',y[1],')')
